801597cbc52aa7eac73b27f998cfbc8546e70a41
manghuang <1255529524@qq.com>
2020-09-04 11:13:45 +0800

    leetcode8 新做的题目

---
+++ src/main/java/leetcode8/Solution0.java
@@ -0,0 +1,56 @@
+package leetcode8;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution0 {
+
+    /*
+        dps+return
+     */
+    public List<String> binaryTreePaths(TreeNode root) {
+
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        return dps(root);
+    }
+
+    private List<String> dps(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        int val = root.val;
+        List<String> leftRes = dps(root.left);
+        List<String> rightRes = dps(root.right);
+        List<String> res = new ArrayList<>();
+        if (leftRes == null && rightRes == null) {
+            res.add(String.valueOf(val));
+        }
+        if (leftRes != null) {
+            for (String str : leftRes
+            ) {
+                res.add(String.valueOf(val) + "->" + str);
+            }
+        }
+        if (rightRes != null) {
+            for (String str : rightRes
+            ) {
+                res.add(String.valueOf(val) + "->" + str);
+            }
+        }
+
+        return res;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+}
