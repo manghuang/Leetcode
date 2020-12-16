7faea72d0b7239e4f1edfae36f7b67d40a0bcbc4
lenovo <1>
2020-12-16 22:15:57 +0800

    Solution2

--- src/main/java/leetcode14/Solution2.java
+++ src/main/java/leetcode14/Solution2.java
@@ -41,4 +41,66 @@ public class Solution2 {
                 "s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s t t");
         System.out.println(res);
     }
+
+    public String solve (String s, String t) {
+        // write code here
+        if(s == null || s.length() == 0){
+            return t;
+        }
+        if(t == null || t.length() == 0){
+            return s;
+        }
+        int sIndex = s.length()-1;
+        int tIndex = t.length()-1;
+        int temp = 0;
+        StringBuilder strBuil = new StringBuilder();
+        while(sIndex >= 0 && tIndex >= 0){
+
+            int num1 = s.charAt(sIndex--) - '0';
+            int num2 = t.charAt(tIndex--) - '0';
+            int res = num1 + num2 + temp;
+            if(res >= 10){
+                temp = 1;
+                res -= 10;
+            }else{
+                temp = 0;
+            }
+            strBuil.append((char)(res + '0'));
+        }
+        while(sIndex >= 0){
+            if(temp  == 0){
+                strBuil.append(s.charAt(sIndex--));
+            }else{
+                int res = s.charAt(sIndex--) - '0';
+                res += temp;
+                if(res >= 10){
+                    temp = 1;
+                    res -= 10;
+                }else{
+                    temp = 0;
+                }
+                strBuil.append((char)(res + '0'));
+            }
+        }
+        while(tIndex >= 0){
+            if(temp == 0){
+                strBuil.append(t.charAt(tIndex--));
+            }else{
+                int res = t.charAt(tIndex--) - '0';
+                res += temp;
+                if(res >= 10){
+                    temp = 1;
+                    res -= 10;
+                }else{
+                    temp = 0;
+                }
+                strBuil.append((char)(res + '0'));
+            }
+        }
+        if(temp == 1){
+            strBuil.append("1");
+        }
+        strBuil.reverse();
+        return strBuil.toString();
+    }
 }
