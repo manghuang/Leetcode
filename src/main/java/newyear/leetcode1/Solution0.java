904bc4777b47ba48a95aaed4956ecd4c751d4788
lenovo <1>
2021-03-07 17:03:06 +0800

    求一个数的二机制表示中1的个数

--- src/main/java/newyear/leetcode1/Solution0.java
+++ src/main/java/newyear/leetcode1/Solution0.java
@@ -5,17 +5,18 @@ public class Solution0 {
     public int[] countBits(int num) {
         int[] ans = new int[num + 1];
         for (int i = 0; i <= num; i++) {
-            ans[i] = get(i);
+//            ans[i] = get(i);
+            ans[i] = Integer.bitCount(i);
         }
         return ans;
     }
 
-    private int get(int num) {
-        int count = 0;
-        while (num != 0){
-            count += num & 1;
-            num = num >> 1;
-        }
-        return count;
-    }
+//    private int get(int num) {
+//        int count = 0;
+//        while (num != 0){
+//            count += num & 1;
+//            num = num >> 1;
+//        }
+//        return count;
+//    }
 }
