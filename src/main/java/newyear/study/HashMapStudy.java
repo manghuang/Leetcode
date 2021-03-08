566cc6e1459f22bbdfdfc7fe02f4f9ce0e4652c6
lenovo <1>
2021-03-08 12:15:20 +0800

    线程池的工作流程和异步计算的通知方式

--- src/main/java/newyear/study/HashMapStudy.java
+++ src/main/java/newyear/study/HashMapStudy.java
@@ -1,31 +1,59 @@
 package newyear.study;
 
-import java.util.HashMap;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class HashMapStudy {
 
     public static void main(String[] args) {
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
-        hashMap.put(1,1);
+//        HashMap<Integer, Integer> hashMap = new HashMap<>();
+//        hashMap.put(1,1);
 
-        ExecutorService service = Executors.newFixedThreadPool(1);
-        for (int i = 0; i < 10; i++) {
-            service.submit(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    System.out.println("线程" + Thread.currentThread().getName());
-                }
-            });
-        }
-//        System.out.println("111");
-        service.shutdown();
+//        ExecutorService service = Executors.newFixedThreadPool(1);
+//        for (int i = 0; i < 5; i++) {
+//            service.execute(new Runnable() {
+//                @Override
+//                public void run() {
+//                    try {
+//                        Thread.sleep(1000);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                    System.out.println("execute:" + "线程" + Thread.currentThread().getName());
+//                }
+//            });
+//        }
+//        Future<Integer> future= service.submit(new Callable<Integer>() {
+//            @Override
+//            public Integer call() throws Exception {
+//                try {
+//                    Thread.sleep(1000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                System.out.println("submit:" + "线程" + Thread.currentThread().getName());
+//                return 1;
+//            }
+//        });
+//
+//        try {
+//            System.out.println(future.get());
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        } catch (ExecutionException e) {
+//            e.printStackTrace();
+//        }
+//        if(future.isDone()){
+//            System.out.println("Done");
+//        }else {
+//            System.out.println("canceled");
+//        }
+//
+//        service.shutdown();
+
+        AtomicInteger atomicInteger = new AtomicInteger(1);
+        boolean b = atomicInteger.compareAndSet(0, 2);
+        System.out.println(b);
 
     }
 }
